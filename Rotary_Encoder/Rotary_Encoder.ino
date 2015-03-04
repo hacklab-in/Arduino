ac8d0051da2a895be1c09618a26e280405ca2831
hacklab-in <hacklab.in@gmail.com>
2015-03-04 11:53:18 +0530

    Rotary Encoder for arduino

---
+++ Rotary_Encoder/Rotary_Encoder.ino
@@ -0,0 +1,35 @@
+ /* Read Quadrature Encoder
+  * Connect Encoder to Pins encoder0PinA, encoder0PinB, and +5V.
+  *
+  * Sketch by max wolf / www.meso.net
+  * v. 0.1 - very basic functions - mw 20061220
+  *
+  */  
+
+
+ int val; 
+ int encoder0PinA = 3;
+ int encoder0PinB = 4;
+ int encoder0Pos = 0;
+ int encoder0PinALast = LOW;
+ int n = LOW;
+
+ void setup() { 
+   pinMode (encoder0PinA,INPUT);
+   pinMode (encoder0PinB,INPUT);
+   Serial.begin (9600);
+ } 
+
+ void loop() { 
+   n = digitalRead(encoder0PinA);
+   if ((encoder0PinALast == LOW) && (n == HIGH)) {
+     if (digitalRead(encoder0PinB) == LOW) {
+       encoder0Pos--;
+     } else {
+       encoder0Pos++;
+     }
+     Serial.print (encoder0Pos);
+     Serial.print ("/");
+   } 
+   encoder0PinALast = n;
+ } 
